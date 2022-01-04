82068f51f4a83b31d1c50fc242adfedfe850441a
Guilherme <gui.vieira.code@gmail.com>
2022-01-04 13:55:58 -0300

    Adiciona componente Profile

---
+++ src/components/profile/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import * as Styled from './styled';
+
+const Profile = () => {
+    return(
+        <Styled.Wrapper>
+            <Styled.WrapperImg src="https://avatars.githubusercontent.com/u/44982023?v=4" 
+                alt="User Avatar"/>
+            <Styled.WrapperUserInfo>
+                <div>
+                    <h1>Guilherme Vieira</h1>
+                    <Styled.WrapperUsername>
+                        <h3>Username:</h3>
+                        <a href="https://github.com/GuilhermeVieiraCode" target="_blank">GuilhermeVieiraCode</a>
+                    </Styled.WrapperUsername>
+                </div>    
+                <Styled.WrapperStatusCount>
+                    <div>
+                    <h4>Followers</h4>
+                    <span>3</span>
+                    </div>
+                    <div>
+                    <h4>Following</h4>
+                    <span>4</span>
+                    </div>
+                    <div>
+                    <h4>Starred</h4>
+                    <span>2</span>
+                    </div>
+                </Styled.WrapperStatusCount>
+           </Styled.WrapperUserInfo> 
+        </Styled.Wrapper>
+    );
+}
+        
+              
+
+export default Profile;
---
+++ src/components/profile/styled.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const WrapperImg = styled.img`
+    border-radius: 50%;
+    width: 15%;
+    margin: 8px;
+`;
+
+export const Wrapper = styled.div`
+    display: flex;
+    align-items: flex-start;
+`;
+
+export const WrapperUserInfo = styled.div`
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+    margin: 8px;
+`;
+
+export const WrapperStatusCount = styled.div`
+    display: flex;
+    align-items: center;
+    div{
+        margin: 8px;
+        text-align: center;
+    }
+`;
+
+export const WrapperUsername = styled.div`
+    display: flex;
+    align-items: center;
+    h3{
+        margin-right: 8px;
+    }
+`;
+
